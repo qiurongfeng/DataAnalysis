e765b18658f309c493872e51198e2faa9de7b783
qiurongfeng <qiurongfeng@pku.edu.cn>
2018-05-04 09:15:59 +0800

    v1.2 2018.5.4

--- main_fuc.py
+++ main_fuc.py
@@ -4,7 +4,7 @@ Created on 2018/4/20 10:27
 author: Rongfeng.Qiu
 file:main_fuc.py
 """
-from DataAnalysis import wordcount,upEndTime,output_excel,key_valuecount,during_timecount,output_pic
+from DataAnalysis import key_valuecount,during_timecount,upEndTime,output_pic,output_excel,wordcount
 import os
 import xlwt
 
@@ -50,34 +50,34 @@ str4 = "End Shot"
 workbook = xlwt.Workbook(encoding='utf-8')
 #############################################################
 
-# ########统计时间差########
-# #输入工作表名称
-# sheetname1 = "sheet 1"
-# #输入初始行
-# initList = ["开机时间","关机时间","时间差（单位秒）"]
-# listTime = upEndTime.count_time(str1,str2,start,end,filepath)
-# workbook = output_excel.output(listTime,workbook,sheetname1,initList)
-# #########################
+########统计时间差########
+#输入工作表名称
+sheetname1 = "sheet 1"
+#输入初始行
+initList = ["开机时间","关机时间","时间差（单位秒）"]
+listTime = upEndTime.count_time(str1,str2,start,end,filepath)
+workbook = output_excel.output(listTime,workbook,sheetname1,initList)
+#########################
 
 
-# ########统计shot时间差########
-# #输入工作表名称
-# sheetname2 = "sheet 2"
-# #输入初始行
-# initList = ["start shot时间","end shot时间","时间差（单位秒）"]
-# shotList = upEndTime.count_time(str3,str4,start,end,filepath)
-# workbook = output_excel.output(shotList,workbook,sheetname2,initList)
-# #########################
+########统计shot时间差########
+#输入工作表名称
+sheetname2 = "sheet 2"
+#输入初始行
+initList = ["start shot时间","end shot时间","时间差（单位秒）"]
+shotList = upEndTime.count_time(str3,str4,start,end,filepath)
+workbook = output_excel.output(shotList,workbook,sheetname2,initList)
+#########################
 
 
-# ########统计出现次数########
-# #输入工作表名称
-# sheetname3 = "sheet 3"
-# #输入初始行
-# initList = ["出现关键字","出现次数"]
-# count_map1 = wordcount.fileListCount(start,end,count_list1,filepath)
-# workbook = output_excel.output(count_map1,workbook,sheetname3,initList)
-# #########################
+########统计出现次数########
+#输入工作表名称
+sheetname3 = "sheet 3"
+#输入初始行
+initList = ["出现关键字","出现次数"]
+count_map1 = wordcount.fileListCount(start,end,count_list1,filepath)
+workbook = output_excel.output(count_map1,workbook,sheetname3,initList)
+#########################
 
 ########统计出现次数########
 #输入工作表名称
@@ -85,31 +85,30 @@ sheetname4 = "sheet 4"
 #输入初始行
 initList = ["出现关键字","出现次数"]
 count_map2 = key_valuecount.fileKeyCount(start,end,count_list2,filepath)
-print(count_map2)
 output_pic.output(count_map2)
-#workbook = output_excel.output_keyvalue(count_map2,workbook,sheetname4,initList)
+workbook = output_excel.output_keyvalue(count_map2,workbook,sheetname4,initList)
 
 #########################
 
-# ########统计出现次数########
-# #输入工作表名称
-# sheetname5 = "sheet 5"
-# #输入初始行
-# initList = ["出现关键字","出现次数"]
-# count_map3 = during_timecount.count_duringtime(str3,str4,start,end,count_list3,filepath)
-# workbook = output_excel.output(count_map3,workbook,sheetname5,initList)
-# #########################
+########统计出现次数########
+#输入工作表名称
+sheetname5 = "sheet 5"
+#输入初始行
+initList = ["出现关键字","出现次数"]
+count_map3 = during_timecount.count_duringtime(str3,str4,start,end,count_list3,filepath)
+workbook = output_excel.output(count_map3,workbook,sheetname5,initList)
+#########################
 
 
 
 # 输入Excel文件名
-excelName = 'res_test.xls'
+excelName = 'res_15.xls'
 
-# try:
-#     workbook.save("output/" + excelName)
-#     print("操作成功,文件" + excelName + "已生成")
-# except OSError as reason:
-#     print("保存失败：" + str(reason))
+try:
+    workbook.save("output/" + excelName)
+    print("操作成功,文件" + excelName + "已生成")
+except OSError as reason:
+    print("保存失败：" + str(reason))
 
 
 
